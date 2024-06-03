594472fa09a176a0735593bd8afb9a66131fe2bb
Jason Le <jason.le@consilium.technology>
2024-06-03 16:05:09 +0930

    added hstack and github

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Box, Tooltip, Link} from '@chakra-ui/react';
+import { Box, Tooltip, Link, HStack} from '@chakra-ui/react';
 import { FaLinkedin } from "react-icons/fa";
 import { IoMailOpen } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa6";
 
 const SocialMedia = ({}) => {
     return (
@@ -13,12 +14,17 @@ const SocialMedia = ({}) => {
         borderRadius="sm"
         textAlign="left"
       >
-        <Tooltip label='https://www.linkedin.com/in/jasonle1402/'>
-            <Link href='https://www.linkedin.com/in/jasonle1402/' color='gray.600'><FaLinkedin /></Link>
-        </Tooltip>
-        <Tooltip label='longle14231@gmail.com'>
-                    <Link href='mailto:longle14231@gmail.com' color='gray.600'><IoMailOpen /></Link>
-                </Tooltip>
+        <HStack>
+            <Tooltip label='https://www.linkedin.com/in/jasonle1402/'>
+                <Link href='https://www.linkedin.com/in/jasonle1402/' color='gray.600'><FaLinkedin size={30}/></Link>
+            </Tooltip>
+            <Tooltip label='longle14231@gmail.com'>
+                <Link href='mailto:longle14231@gmail.com' color='gray.600'><IoMailOpen size={30}/></Link>
+            </Tooltip>
+            <Tooltip label='https://github.com/JasonLee0404'>
+                <Link href='https://github.com/JasonLee0404' color='gray.600'><FaGithub size={30}/></Link>
+            </Tooltip>
+        </HStack>
       </Box>
     );
   };
